55e9be6d0845454ff051692f45841f35a457e70e
Philip May'r <philipmayr@outlook.com>
2023-07-11 00:54:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,679 @@
+let questions = [
+    {
+        question: "A random access, variable-size data structure that allows elements to be added or removed is a random array.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true }
+        ]
+    },
+    {
+        question: "Good programming practice follows the DRIP principle.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true }
+        ]
+    },
+    {
+        question: "The first action in defining a while loop is to test an expression.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true }
+        ]
+    },
+    {
+        question: "In software testing, test data should be verified by checking the pseudocode to see if it produces correct results.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true }
+        ]
+    },
+    {
+        question: "In the book examples, output functions typically had no parameters in and a return value out.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true }
+        ]
+    },
+    {
+        question: "The value of True AND True is _____.",
+        answers: [
+            { text: "true" },
+        ]
+    },
+    {
+        question: "Evaluating a string literal containing one or more placeholders, yielding a result in which the placeholders are replaced with their corresponding values is _____.",
+        answers: [
+            { text: "interpolation" },
+        ]
+    },
+    {
+        question: "_____ is used to identify a block of code in Python.",
+        answers: [
+            { text: "Indentation" },
+        ]
+    },
+    {
+        question: "The data type of all input is _____.",
+        answers: [
+            { text: "string" },
+        ]
+    },
+    {
+        question: "_____ is a branching statement that causes a function to jump back to the function that called it.",
+        answers: [
+            { text: "Return" },
+        ]
+    },
+    {
+        question: "An anomaly where a program overwrites adjacent memory locations is a _____.",
+        answers: [
+            { text: "buffer overflow" },
+        ]
+    },
+    {
+        question: "_____ is a test before loop typically used when the number of iterations is known before entering the loop.",
+        answers: [
+            { text: "For" },
+        ]
+    },
+    {
+        question: "The data type of the value &quot;3.14&quot;.",
+        answers: [
+            { text: "string" },
+        ]
+    },
+    {
+        question: "_____ is a two-way selection control structure.",
+        answers: [
+            { text: "if-then-else" },
+        ]
+    },
+    {
+        question: "Standard input stream, typically the keyboard is _____.",
+        answers: [
+            { text: "stdin" },
+        ]
+    },
+    {
+        question: "A _____ is a character array.",
+        answers: [
+            { text: "string" },
+        ]
+    },
+    {
+        question: "A widely used approach in systems analysis and software engineering for describing the structure of an information processing program or other process is known as XML.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true }
+        ]
+    },
+    {
+        question: "The assignment operator is _____.",
+        answers: [
+            { text: "=" },
+        ]
+    },
+    {
+        question: "The _____ flowcharting symbol is used to designate input and output operations.",
+        answers: [
+            { text: "parallelogram" },
+        ]
+    },
+    {
+        question: "An _____ is a value provided as input to a function.",
+        answers: [
+            { text: "argument" },
+        ]
+    },
+    {
+        question: "Standard output error stream, typically the monitor is _____.",
+        answers: [
+            { text: "stderr" },
+        ]
+    },
+    {
+        question: "During the _____ phase of the System Development Life Cycle, the programmers would be assigned to write the specific programs using a programming language decided by the System Analyst.",
+        answers: [
+            { text: "implementation" },
+        ]
+    },
+    {
+        question: "Maximum numeric range of a 32-bit integer is 2,147,483,647.",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "Variables created as part of a program's stack are _____.",
+        answers: [
+            { text: "local" },
+        ]
+    },
+    {
+        question: "The value of False AND False is _____.",
+        answers: [
+            { text: "False" },
+        ]
+    },
+    {
+        question: "The result of reversing the string &quot;Hello&quot; is _____.",
+        answers: [
+            { text: "olleH" },
+        ]
+    },
+    {
+        question: "Software testing includes verifying that the program responds correctly to all kinds of inputs.",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "_____ is the index value of the second element in an array.",
+        answers: [
+            { text: "1" },
+        ]
+    },
+    {
+        question: "Define the data format and available procedures for a given type of object.",
+        answers: [
+            { text: "Classes" },
+        ]
+    },
+    {
+        question: "In software testing, test data should be verified by checking the pseudocode to see if it produces correct results.",
+        answers: [
+            { text: "True", correct: false },
+            { text: "False", correct: true }
+        ]
+    },
+    {
+        question: "A software application that provides comprehensive facilities to computer programmers for software development.",
+        answers: [
+            { text: "IDE" },
+        ]
+    },
+    {
+        question: "The relational operator meaning greater than is _____.",
+        answers: [
+            { text: ">" },
+        ]
+    },
+    {
+        question: "A function's communications declaration to a compiler is known as the _____.",
+        answers: [
+            { text: "function prototype" },
+        ]
+    },
+    {
+        question: "The result of finding the first index of &quot;l&quot; in &quot;Hello&quot; is _____.",
+        answers: [
+            { text: "2" },
+        ]
+    },
+    {
+        question: "_____ is a multi-way selection control structure.",
+        answers: [
+            { text: "Case" },
+        ]
+    },
+    {
+        question: "The most popular version control system is _____.",
+        answers: [
+            { text: "Git" },
+        ]
+    },
+    {
+        question: "Variables created inside a function are _____ in scope.",
+        answers: [
+            { text: "local" },
+        ]
+    },
+    {
+        question: "Maximum number of digits in a 64-bit floating-point value is _____.",
+        answers: [
+            { text: "15" },
+        ]
+    },
+    {
+        question: "A _____ is a single-dimension array.",
+        answers: [
+            { text: "list" },
+        ]
+    },
+    {
+        question: "_____ are used to identify a block of code in languages such as C++, C#, Java, and JavaScript.",
+        answers: [
+            { text: "Braces" },
+        ]
+    },
+    {
+        question: "_____ is a form of implicit data structure that uses multiple arrays to represent a singular array of data.",
+        answers: [
+            { text: "Parallel arrays" },
+        ]
+    },
+    {
+        question: "_____ is a control structure where the program chooses between two or more options.",
+        answers: [
+            { text: "Selection" },
+        ]
+    },
+    {
+        question: "_____ is a branching statement that terminates the existing structure.",
+        answers: [
+            { text: "Break" },
+        ]
+    },
+    {
+        question: "A specification that defines a procedure or behavior of an object.",
+        answers: [
+            { text: "method" },
+        ]
+    },
+    {
+        question: "_____ describes a loop with no terminating condition typically used when the number of iterations is NOT known before entering the loop.",
+        answers: [
+            { text: "Infinite" },
+        ]
+    },
+    {
+        question: "A method used to return the value of a private member variable, also known as a getter method.",
+        answers: [
+            { text: "Accessor" },
+        ]
+    },
+    {
+        question: "The result of trimming &quot; &quot; from &quot;Hello world&quot; is _____.",
+        answers: [
+            { text: "Hello world" },
+        ]
+    },
+    {
+        question: "A _____ causes a computer to begin executing a different instruction sequence.",
+        answers: [
+            { text: "branch" },
+        ]
+    },
+    {
+        question: "An interpreted high-level programming language for general purpose programming is _____.",
+        answers: [
+            { text: "Python" },
+        ]
+    },
+    {
+        question: "In _____ case, each word is lowercase with underscores separating words.",
+        answers: [
+            { text: "snake" },
+        ]
+    },
+    {
+        question: "_____ is a predefined function used to prematurely stop a program and return to the operating system.",
+        answers: [
+            { text: "Exit" },
+        ]
+    },
+    {
+        question: "_____ describes the amount of system resources used by a program.",
+        answers: [
+            { text: "Efficiency" },
+        ]
+    },
+    {
+        question: "The first action in defining a do loop is test an expression.",
+        answers: [
+            { text: "false" },
+        ]
+    },
+    {
+        question: "An iteration control structure that places one loop inside another loop is known as _____.",
+        answers: [
+            { text: "nesting" },
+        ]
+    },
+    {
+        question: "Keyword used in some computer programming languages to refer to the object, class, or other entity that the currently running code is part of.",
+        answers: [
+            { text: "self" },
+        ]
+    }
+    /*
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    v
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    },
+    {
+        question: "",
+        answers: [
+            { text: "" },
+        ]
+    }
+    */
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-btns");
+const nextButton = document.getElementById("next-btn");
+const input = document.getElementById("input");
+const checkButton = document.getElementById("check-btn");
+const hintButton = document.getElementById("hint-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    hintButton.style.display = "none";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNum = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNum + ". " + currentQuestion.question;
+    
+    if (currentQuestionIndex > 0){
+        input.placeholder = "";
+    }
+
+    if (currentQuestion.answers.length === 1){
+        input.style.display = "block";
+        input.focus();
+        checkButton.style.display = "block";
+    }else{
+        currentQuestion.answers.forEach(answer => {
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("btn");
+            answerButtons.appendChild(button);
+            if(answer.correct){
+                button.dataset.correct = answer.correct;
+            }
+            button.addEventListener("click", selectAnswer);
+        });
+    }
+}
+
+function resetState(){
+    input.style.color = '#03254c';
+    input.value = "";
+    counter = 0;
+    hint = "";
+    checkButton.innerHTML = "Check";
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (input.value === ""){
+        console.log("input empty")
+    }
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }   
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", () => {
+    input.disabled = false;
+    checkButton.style.display = "none";
+    input.style.display = "none";
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+checkButton.addEventListener("click", () => {
+    const isCorrect = input.value.trim() == questions[currentQuestionIndex].answers[0].text;
+    if (isCorrect) {
+        score++;
+        checkButton.style.display = "none";
+        nextButton.style.display = "block";
+        input.style.borderColor = '#008000';
+        hintButton.style.display = "none";
+        input.style.color = '#008000';
+        input.disabled = true;
+    }else{
+        input.style.borderColor = 'grey';
+        input.style.color = 'grey';
+        hintButton.style.display = "block";
+    }
+});
+
+counter = 0;
+hint = "";
+
+input.addEventListener("keyup", event => {
+    if(event.key !== "Enter") return;
+    checkButton.click();
+    event.preventDefault();
+});
+
+hintButton.addEventListener("click", () => {
+    if (counter < questions[currentQuestionIndex].answers[0].text.length){
+        input.style.color = '#03254c';
+        hint += questions[currentQuestionIndex].answers[0].text[counter];
+        input.value = hint;
+        input.focus();
+        counter++;
+        if (counter == questions[currentQuestionIndex].answers[0].text.length){
+            input.style.color = '#008000';
+            input.style.borderColor = '#008000';
+            checkButton.style.display = "none";
+            nextButton.style.display = "block";
+            hintButton.style.display = "none";
+            input.disabled = true;
+            score++;
+        }
+    }
+});
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You got ${score} out of ${questions.length} questions right.`;
+    nextButton.innerHTML = "Restart Review";
+    nextButton.style.display = "block";
+    score = 0;
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+startQuiz();
